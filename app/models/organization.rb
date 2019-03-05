923e56edd4313a22081dafe0a75a5d82c4426457
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2019-03-05 15:06:47 +0100

    Add Organization#quality_frequency_distribution method

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,9 @@
 
 class Organization < ActiveRecord::Base
   has_many :branches
+  has_many :feedbacks, through: :branches
+
+  def quality_frequency_distribution
+    feedbacks.group(:quality).count
+  end
 end
---
+++ test/models/organization_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class OrganizationTest < ActiveSupport::TestCase
+  test 'it has following attributes' do
+    expected_attributes = %w[id name created_at updated_at]
+
+    assert_equal expected_attributes, Organization.column_names
+  end
+
+  test 'computes quality frequency distribution for organization' do
+    organization = Organization.create(name: 'Some organisation')
+
+    branch_1 = Branch.create(name: 'Some branch', organization: organization)
+    Feedback.create(branch: branch_1, quality: 1)
+    Feedback.create(branch: branch_1, quality: 4)
+
+    branch_2 = Branch.create(name: 'Another branch', organization: organization)
+    Feedback.create(branch: branch_2, quality: 4)
+    Feedback.create(branch: branch_2, quality: 5)
+
+
+    expected_distribution = { 1 => 1, 4 => 2, 5 => 1 }
+
+    assert_equal expected_distribution, organization.quality_frequency_distribution
+  end
+end
